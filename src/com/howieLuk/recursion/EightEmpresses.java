578db65772b26e09c0026de5e645319e01d7a1f1
HowieLuk <2681701269@qq.com>
2024-01-04 02:54:50 +0800

    八皇后问题

--- src/com/howieLuk/recursion/EightEmpresses.java
+++ src/com/howieLuk/recursion/EightEmpresses.java
@@ -8,6 +8,38 @@ package com.howieLuk.recursion;
  **/
 public class EightEmpresses {
 
+    private static int[] empresses = new int[9];
 
+    public static int getAnswer() {
+        empress(1);
+        return empresses[0];
+    }
+
+    private static void empress(int e) {
+        if (e == empresses.length) {
+            empresses[0]+=1;
+            return;
+        }
+        for (int i = 1; i < empresses.length; i++) {
+            empresses[e] = i;
+            if (noConflict(e)) {
+                empress(e + 1);
+            }
+        }
+    }
+
+    private static boolean noConflict(int e) {
+        for (int i = e - 1; i > 0; i--) {
+            if (i != e && (empresses[i] == empresses[e] ||
+                    Math.abs(empresses[i] - empresses[e]) == Math.abs(i - e))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getAnswer());
+    }
 
 }
