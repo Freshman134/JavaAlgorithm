91c9194fca7d15142667b08d6eaa66f4c33ea89f
HowieLuk <2681701269@qq.com>
2024-01-22 02:26:16 +0800

    KMP算法

--- src/com/howieLuk/algorithm/KMP.java
+++ src/com/howieLuk/algorithm/KMP.java
@@ -5,4 +5,35 @@ package com.howieLuk.algorithm;
  * @date 2024/1/21 3:13
  */
 public class KMP {
+    public static int match(String str, String pat) {
+        int[] kmpArr = getKMPArray(pat);
+
+        int s = 0, p = 0;
+        while (p < pat.length() && s < str.length()) {
+            if (str.charAt(p) == str.charAt(s)) {
+                p++; s++;
+            } else {
+                p = kmpArr[p > 0 ? 0 : p - 1];
+            }
+        }
+        if (p == pat.length()) {
+            return s - p;
+        }
+        return -1;
+    }
+
+    private static int[] getKMPArray(String pat) {
+        int[] retArr = new int[pat.length()];
+        int kmp;
+        for (int i = 0; i < retArr.length; i++) {
+            kmp = i + 1;
+            if (pat.charAt(i) == pat.charAt(kmp)) {
+                retArr[kmp] = retArr[i] + 1;
+            } else {
+                retArr[kmp] = 0;
+            }
+        }
+        return retArr;
+    }
+
 }
--- src/com/howieLuk/leetcode/Rotate.java
+++ src/com/howieLuk/leetcode/Rotate.java
@@ -9,14 +9,22 @@ import java.util.Arrays;
 public class Rotate {
 
     static public void rotate(int[] nums, int k) {
-        int rotateElem = nums[0], ind = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int next = (ind + k) % nums.length;
-            int tmp = nums[next];
-            nums[next] = rotateElem;
-
-            ind = next;
+        int rotateElem, ind;
+        int count = gcd(k, nums.length);
+        for (int i = 0; i < count; i++) {
+            ind = i; rotateElem = nums[i];
+            do {
+                int next = (ind + k) % nums.length;
+                int tmp = nums[next];
+                nums[next] = rotateElem;
+                rotateElem = tmp;
+                ind = next;
+            } while (ind != i);
         }
+    }
+
+    static public int gcd(int x, int y) {
+        return y > 0 ? gcd(y, x % y) : x;
     }
 
     public static void main(String[] args) {
